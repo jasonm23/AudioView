4865a4b868bf906a56c3e71e7b3e216620f0ab44
Jason Milkins <jasonm23@gmail.com>
2023-04-10 19:59:41 +0700

    Initial Commit

---
+++ AudioView/AudioViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioViewApp.swift
+//  AudioView
+//
+//  Created by jason on 10/4/23.
+//
+
+import SwiftUI
+
+@main
+struct AudioViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AudioView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AudioView
+//
+//  Created by jason on 10/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
